a24f12ab1a3ef18395ca842848cf513f0d359af1
Jeyson saraiva <jeysonsaraiva@gmail.com>
2022-10-29 01:22:11 -0300

    Página específica dos personagens e página de login

---
+++ links/index.js
@@ -0,0 +1,12 @@
+
+const botao = document.getElementById("btnADC")
+
+const lipvp = document.getElementById("lipvp").addEventListener("click", remove)
+
+const lipve = document.getElementById("lipve").addEventListener("click", remove)
+
+
+function remove(){
+    botao.classList.remove("d-none")
+
+}
