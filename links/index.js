917e9e2fe2cb97f2b7867de104e1be34e416c07a
Jeyson saraiva <jeysonsaraiva@gmail.com>
2022-10-31 09:28:45 -0300

    Resto das páginas específicas

--- links/index.js
+++ links/index.js
@@ -5,8 +5,11 @@ const lipvp = document.getElementById("lipvp").addEventListener("click", remove)
 
 const lipve = document.getElementById("lipve").addEventListener("click", remove)
 
+const divmsg = document.getElementById("divmsg")
+
 
 function remove(){
     botao.classList.remove("d-none")
+    divmsg.classList.add("d-none")
 
 }
